2325d8c0a2cd510794aefbf5c161c15bafedf317
Vegvizer <tal.vegvizer@gmail.com>
2017-08-13 13:29:24 +0300

    add new Chat message system

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+    var socket = io();
+
+    socket.on('connect', function () {
+      console.log('Connected to server');
+
+        socket.emit('createMessage', {
+            from:"Tal",
+            text: "Yup that works for me"
+        });
+
+    });
+
+    socket.on('disconnect', function () {
+      console.log('Disconnected from server');
+    });
+
+    socket.on('newMessage', function (message) {
+        console.log('newMessage', message);
+    });
+
+
+    
+  
